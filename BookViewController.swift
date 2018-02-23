cf4f23b4d940a33402149d772bd57b4a67d2713d
Matthew Ivkovich <ivkovmg@auburn.edu>
2018-02-22 22:29:03 -0500

    imageViews completed

---
+++ BookViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BookViewController.swift
+//  BookInventory
+//
+//  Created by Matthew Ivkovich on 2/22/18.
+//  Copyright © 2018 Mivko Tech. All rights reserved.
+//
+
+import UIKit
+
+class BookViewController: UIViewController {
+
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
